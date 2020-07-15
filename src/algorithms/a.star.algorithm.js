2cd2be54df4021aa79c5e93183f1fb90e0ed4bb7
javierOnt <javieront2000@gmail.com>
2020-07-14 19:37:14 -0500

    added code to main algo function

--- src/algorithms/a.star.algorithm.js
+++ src/algorithms/a.star.algorithm.js
@@ -7,17 +7,22 @@ export const aStarAlgorithm = (start, end) => {
   openSet.push(start);
   if (openSet.length > 0) {
     // continue searching
-    let bestNode = 0;
+    let closer = 0;
 
     for (let i = 0; i < openSet.length; i++) {
-      if (openSet[i].f < openSet[bestNode].f) {
-        bestNode = i;
+      if (openSet[i].f < openSet[closer].f) {
+        closer = i;
       }
     }
 
-    if (openSet[bestNode] === end) {
+    let current = openSet[closer];
+
+    if (current === end) {
       console.log("FOUND!!!");
     }
+
+    openSet.splice(openSet.indexOf(current), 1);
+    closedSet.push(current);
   } else {
     // it's done
   }
