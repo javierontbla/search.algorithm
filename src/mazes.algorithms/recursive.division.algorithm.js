fbfef7715da3bd8283c7a6831837d7096a818f4c
javierOnt <javieront2000@gmail.com>
2020-07-24 14:37:04 -0500

    recursive algorithm refactor

--- src/mazes.algorithms/recursive.division.algorithm.js
+++ src/mazes.algorithms/recursive.division.algorithm.js
@@ -18,18 +18,51 @@ const addBorderWalls = (grid, cols, rows) => {
   return grid;
 };
 
-const addWallEntrance = () => {};
+const addHWall = (minX, maxX, y, grid) => {
+  const door = randomNumber(minX, maxX);
 
-const addInnerWalls = () => {};
+  for (let i = minX; i <= maxX; i++) {
+    if (i === door) grid[y][i].obstacle = false;
+    else grid[y][i].obstacle = true;
+  }
+};
+
+const addVWall = (minX, maxX, x, grid) => {
+  const door = randomNumber(minX, maxX);
+
+  for (let i = minX; i <= maxX; i++) {
+    if (i === door) grid[i][x].obstacle = false;
+    else grid[i][x].obstacle = true;
+  }
+};
+
+const addInnerWalls = (first, minX, maxX, minY, maxY, grid) => {
+  if (first) {
+    if (maxX - minX < 2) {
+      return;
+    }
+
+    const y = randomNumber(minY, maxY);
+    addHWall(minX, maxX, y, grid);
+
+    addInnerWalls(!first, minX, maxX, minY, y - 1);
+    addInnerWalls(!first, minX, maxX, y + 1, maxY);
+  } else {
+    if (maxY - minY < 2) {
+      return;
+    }
+
+    const x = randomNumber(minX, maxX);
+    addVWall(minY, maxY, x, grid);
+
+    addInnerWalls(!first, minX, x - 1, minY, maxY);
+    addInnerWalls(!first, x + 1, maxX, minY, maxY);
+  }
+};
 
 export const recursiveDivision = (grid, cols, rows) => {
   // generating the border walls of the grid
-  addBorderWalls(grid, cols, rows);
-
-  // adding entrances
-  const entrance = addWallEntrance();
+  const borderGrid = addBorderWalls(grid, cols, rows);
   // the walls that actually form the maze
-  addInnerWalls();
+  addInnerWalls(true, 1, grid.length - 2, 1, grid.length - 2, borderGrid);
 };
-
-// https://stackoverflow.com/questions/23530756/maze-recursive-division-algorithm-design
