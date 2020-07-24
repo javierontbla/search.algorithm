bec941f6d3aeaf4cc50b78dbbd9b861deba8805d
javierontbla <javieront2000@gmail.com>
2020-07-24 16:49:23 -0500

    middle algorithm

--- src/mazes.algorithms/recursive.division.algorithm.js
+++ src/mazes.algorithms/recursive.division.algorithm.js
@@ -14,55 +14,57 @@ const addBorderWalls = (grid, cols, rows) => {
       grid[i][rows - 1].obstacle = true;
     }
   }
-
   return grid;
 };
 
-const addHWall = (minX, maxX, y, grid) => {
-  const door = randomNumber(minX, maxX);
-
-  for (let i = minX; i <= maxX; i++) {
-    if (i === door) grid[y][i].obstacle = false;
-    else grid[y][i].obstacle = true;
-  }
-};
-
-const addVWall = (minX, maxX, x, grid) => {
-  const door = randomNumber(minX, maxX);
-
-  for (let i = minX; i <= maxX; i++) {
-    if (i === door) grid[i][x].obstacle = false;
-    else grid[i][x].obstacle = true;
-  }
-};
-
-const addInnerWalls = (first, minX, maxX, minY, maxY, grid) => {
-  if (first) {
-    if (maxX - minX < 2) {
-      return;
-    }
-
-    const y = randomNumber(minY, maxY);
-    addHWall(minX, maxX, y, grid);
-
-    addInnerWalls(!first, minX, maxX, minY, y - 1);
-    addInnerWalls(!first, minX, maxX, y + 1, maxY);
-  } else {
-    if (maxY - minY < 2) {
-      return;
-    }
-
-    const x = randomNumber(minX, maxX);
-    addVWall(minY, maxY, x, grid);
-
-    addInnerWalls(!first, minX, x - 1, minY, maxY);
-    addInnerWalls(!first, x + 1, maxX, minY, maxY);
-  }
-};
-
-export const recursiveDivision = (grid, cols, rows) => {
+export const recursiveDivision = (graph, cols, rows) => {
   // generating the border walls of the grid
-  const borderGrid = addBorderWalls(grid, cols, rows);
-  // the walls that actually form the maze
-  addInnerWalls(true, 1, grid.length - 2, 1, grid.length - 2, borderGrid);
+  // this returns the grid with border walls
+  let grid = addBorderWalls(graph, cols, rows);
+
+  const addHWall = (minX, maxX, y, grid) => {
+    const door = randomNumber(minX, maxX);
+
+    for (let i = minX; i <= maxX; i++) {
+      if (i === door) grid[y][i].maze = false;
+      else grid[y][i].maze = true;
+    }
+  };
+
+  const addVWall = (minX, maxX, x, grid) => {
+    const door = randomNumber(minX, maxX);
+
+    for (let i = minX; i <= maxX; i++) {
+      if (i === door) grid[i][x].maze = false;
+      else grid[i][x].maze = true;
+    }
+  };
+
+  const addInnerWalls = (first, minX, maxX, minY, maxY) => {
+    if (first) {
+      if (maxX - minX < 2) {
+        return;
+      }
+
+      const y = randomNumber(minY, maxY);
+      addHWall(minX, maxX, y, grid);
+
+      addInnerWalls(!first, minX, maxX, minY, y - 1);
+      addInnerWalls(!first, minX, maxX, y + 1, maxY);
+    } else {
+      if (maxY - minY < 2) {
+        return;
+      }
+
+      const x = randomNumber(minX, maxX);
+      addVWall(minY, maxY, x, grid);
+
+      addInnerWalls(!first, minX, x - 1, minY, maxY);
+      addInnerWalls(!first, x + 1, maxX, minY, maxY);
+    }
+  };
+
+  // the walls that actually form the maze (inside)
+  addInnerWalls(true, 1, graph.length - 2, 1, graph.length - 2);
+  return grid;
 };
